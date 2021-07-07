60bb69312b58d2b5a7fc432330b3e5672b5b9057
Pepe <jp@jin.dev>
2021-07-07 14:51:51 +0800

    wip: add native code

---
+++ ios/Classes/native_add.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Pepe on 2021/7/6.
+//
+
+#include <stdint.h>
+
+extern "C" __attribute__((visibility("default"))) __attribute__((used))
+int32_t native_add(int32_t x, int32_t y) {
+    return x + y;
+}
